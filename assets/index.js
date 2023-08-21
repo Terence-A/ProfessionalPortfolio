7c1730ed5c6d170f450b2284502ee411c57763ac
terenceDev <terence.arseneault@hotmail.com>
2023-08-21 16:35:49 -0300

    added links to the projects to the github page

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+const glamping = document.querySelector("#glamping");
+const whatsForDinner = document.querySelector("#whatsForDinner");
+const weatherApp = document.querySelector("#weatherApp");
+const employeeTracker = document.querySelector("#employeeTracker");
+const workDay = document.querySelector("#workDay");
+
+console.log(whatsForDinner);
+
+glamping.addEventListener("click", () => {
+  location.href = "https://github.com/Terence-A/Atlantic-Glamping";
+});
+whatsForDinner.addEventListener("click", () => {
+  location.href = "https://github.com/Terence-A/WhatsForDinner";
+});
+weatherApp.addEventListener("click", () => {
+  location.href = "https://github.com/Terence-A/WeatherDashboard";
+});
+employeeTracker.addEventListener("click", () => {
+  location.href = "https://github.com/Terence-A/EmployeeManagementTracker";
+});
+workDay.addEventListener("click", () => {
+  location.href = "https://github.com/Terence-A/workDayScheduler";
+});
